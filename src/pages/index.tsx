f5be288d69a2715b9e415ad60203d3ca8ba4677e
Pedro Oliveira <pedrodepaivasoaresll@gmail.com>
2020-10-12 18:41:49 -0300

    chore: server side rendering

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,9 +1,39 @@
+import { GetServerSideProps } from "next";
 import { Title } from "../styles/pages/Home";
 
-export default function Home() {
+interface IProduct {
+  id: string;
+  title: string;
+}
+
+interface HomeProps {
+  recommendedProducts: IProduct[];
+}
+
+export default function Home({ recommendedProducts }: HomeProps) {
   return (
     <div>
-      <Title>Hello World</Title>
+      <section>
+        <Title>Products</Title>
+
+        <ul>
+          {recommendedProducts.map((product) => {
+            return <li key={product.id}>{product.title}</li>;
+          })}
+        </ul>
+      </section>
     </div>
   );
 }
+
+export const getServerSideProps: GetServerSideProps<HomeProps> = async () => {
+  const data = await fetch("http://localhost:3333/recommended").then<
+    IProduct[]
+  >((response) => response.json());
+
+  return {
+    props: {
+      recommendedProducts: data,
+    },
+  };
+};
