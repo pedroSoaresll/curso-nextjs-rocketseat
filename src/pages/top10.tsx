64fa596af3e79d654468f23b1000bdaff95f1a0d
Pedro Oliveira <pedrodepaivasoaresll@gmail.com>
2020-10-12 19:14:12 -0300

    feature: generating static pages

---
+++ src/pages/top10.tsx
@@ -0,0 +1,36 @@
+import { GetStaticProps } from "next";
+
+interface IProducts {
+  id: string;
+  title: string;
+}
+
+interface Top10Props {
+  products: IProducts[];
+}
+
+export default function Top10({ products }: Top10Props) {
+  return (
+    <div>
+      <h1>Top 10</h1>
+
+      <ul>
+        {products.map((product) => (
+          <li key={product.id}>{product.title}</li>
+        ))}
+      </ul>
+    </div>
+  );
+}
+
+export const getStaticProps: GetStaticProps<Top10Props> = async ({}) => {
+  const response = await fetch("http://localhost:3333/products");
+  const products = await response.json();
+
+  return {
+    props: {
+      products,
+    },
+    revalidate: 5,
+  };
+};
